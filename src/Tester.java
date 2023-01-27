a2a0846fb7552039fd64723bb5e6000f5faedc81
Eduard <eduard.prog22@gmail.com>
2023-01-27 15:07:46 +0100

    First Try

---
+++ src/Tester.java
@@ -0,0 +1,11 @@
+public class Tester {
+    public Tester(){
+    }
+    public static void main(String[] args){
+
+        String string = "are you able to climb, are you able to swim or are you able to fly?";
+
+        System.out.println(string.replaceAll(" are", "_XYZ"));
+
+    }
+}
